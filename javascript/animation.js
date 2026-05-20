ad3d59d11b99ce653bc2ddc81f309a2e0ea35cee
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-05-20 16:51:11 +0200

    feat: improving fade-in-animations for the images of the hero section on the main page

--- javascript/animation.js
+++ javascript/animation.js
@@ -5,12 +5,15 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("image-wrapper");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.opacity = "0";
+    slides[i].style.transition = "opacity 0.8s ease-in-out";
+    // slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1
-  }    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 3000);
+  }  
+  slides[slideIndex-1].style.opacity = "1";
+  // slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000);
 }
